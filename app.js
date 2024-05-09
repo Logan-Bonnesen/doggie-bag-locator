2227adf14b36c8a9ad7d63f8f6146c1fd75b0ecb
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2024-05-08 21:01:33 -0600

    adding petRoutes routing to app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path')
 const sequelize = require('./server/config/database.js')
 
 const userRoutes = require('./server/routes/userRoutes.js')
-
+const petRoutes = require('./server/routes/petRoutes.js')
 
 const PORT = process.env.PORT || 3000;
 
@@ -19,12 +20,12 @@ sequelize.sync({ force: false })
 // middleware
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    res.send('123456')
-})
+// Express serving index.html from 'public'
+app.use(express.static(path.join(__dirname, 'public')))
 
 // mounting routes
 app.use('/api/users', userRoutes)
+app.use('/api/pets', petRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
