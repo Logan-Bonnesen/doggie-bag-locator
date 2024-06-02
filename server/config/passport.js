acc5d4dda51e69dfd03f6805cadb76c55866c25a
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2024-06-02 14:06:00 -0600

    adding passport.js for authentication

---
+++ server/config/passport.js
@@ -0,0 +1,39 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt')
+const { User } = require('../models')
+
+const initializePassport = (passport) => {
+    passport.use(
+        new LocalStrategy({ usernameField: 'email'}, (email, password, done) => {
+            User.findOne({ where: { email }})
+            .then(user => {
+                if (!user) {
+                    return done(null, false, { message: 'Password or email incorrect'})
+                }
+
+                bcrypt.compare(password, user.password)
+                    .then(isMatch => {
+                        if (isMatch) {
+                            return done(null, user)
+                        } else {
+                            return done(null, false, { message: 'Password or email incorrect'})
+                        }
+                    })
+                    .catch(err => done(err))
+            })
+            .catch(err => done(err))
+        })
+    )
+
+    passport.serializedUser((user, done) => {
+        done(null, user.id)
+    })
+
+    passport.deserializedUser((id, done) => {
+        User.findByPk(id)
+            .then(user => done(null, user))
+            .catch(err => done(err))
+    })
+}
+
+module.exports = initializePassport
