b510c00e462940d0d6bbd555205fa6a022188fbd
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2024-06-02 14:51:43 -0600

    adding routes and middleware for auth, initializing passport

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 const path = require('path')
 const sequelize = require('./server/config/database.js')
 const { engine } = require('express-handlebars')
+const session = require('express-session')
+const flash = require('connect-flash')
+const passport = require('passport')
+const initializePassport = require('./server/config/passport.js')
+
+
 
 const userRoutes = require('./server/routes/userRoutes.js')
 const petRoutes = require('./server/routes/petRoutes.js')
 const locationRoutes = require('./server/routes/locationRoutes.js')
 const commentRoutes = require('./server/routes/commentRoutes.js')
+const authRoutes = require('./server/routes/authRoutes.js')
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // configure handlebars
@@ -26,15 +33,44 @@ sequelize.sync({ force: false })
 
 // middleware
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}))
 
 // Express serving from 'public'
 // app.use(express.static(path.join(__dirname, 'public')))
+
+// express session
+// ******************************************************************************************************************************************************************************
+// change secret below
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true
+}))
+
+// initialize passport
+initializePassport(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
+// connect flash
+app.use(flash());
+
+// global variables to use flash in all views
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  res.locals.error = req.flash('error')
+  next()
+})
 
 // mounting routes
 app.use('/api/users', userRoutes)
 app.use('/api/pets', petRoutes)
 app.use('/api/locations', locationRoutes)
 app.use('/api/comments', commentRoutes)
+app.use('/api/auth', authRoutes)
+
+
 
 app.get('/', (req, res) => {
   res.render('home', { title: 'Home'})
