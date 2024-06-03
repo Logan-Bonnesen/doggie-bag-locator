175995b52809741df4fecbe5a0e644fd11746ec3
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2024-06-03 16:07:12 -0600

    adding index.js to /models for simpler import and export

---
+++ server/models/index.js
@@ -0,0 +1,12 @@
+const User = require('./User');
+const Pet = require('./Pet');
+const Comment = require('./Comment');
+const Location = require('./Location');
+
+module.exports = {
+    User,
+    Pet,
+    Comment,
+    Location
+};
+ 
